e206ab1ca6176db4a7b7d52dbe5efb9e96599c96
Gleb Karelin <s33244@pjwstk.edu.pl>
2026-03-25 22:58:07 +0100

    Add user types: Student (limit 2) and Employee (limit 5)

---
+++ apbd-cw3-s33244/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace apbd_cw3_s33244.Models;
+
+public class Employee : User
+{
+    public string EmployeeId { get; set; }
+    public string Department { get; set; }
+    public override int MaxActiveRentals => 5;
+    
+    public Employee(string firstName, string lastName, string employeeId, string department) 
+        : base(firstName, lastName)
+    {
+        EmployeeId = employeeId;
+        Department = department;
+    }
+}
---
+++ apbd-cw3-s33244/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw3_s33244.Models;
+
+public class Student : User
+{
+    public string StudentId { get; set; }
+    public override int MaxActiveRentals => 2;
+    
+    public Student(string firstName, string lastName, string studentId) 
+        : base(firstName, lastName)
+    {
+        StudentId = studentId;
+    }
+}

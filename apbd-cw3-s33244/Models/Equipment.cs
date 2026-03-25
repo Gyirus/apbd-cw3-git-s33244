4f6fdbb9d0b064969690204ee668d4b89b1d065a
Gleb Karelin <s33244@pjwstk.edu.pl>
2026-03-25 22:52:00 +0100

    Add base abstract classes Equipment and User

---
+++ apbd-cw3-s33244/Models/Equipment.cs
@@ -0,0 +1,14 @@
+﻿namespace apbd_cw3_s33244.Models;
+
+public class Equipment
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; } = true;
+    public bool IsDamaged { get; set; } = false;
+    
+    protected Equipment(string name)
+    {
+        Name = name;
+    }
+}
---
+++ apbd-cw3-s33244/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace apbd_cw3_s33244.Models;
+
+public abstract class User
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public abstract int MaxActiveRentals { get; }
+    
+    protected User(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    
+    public string FullName => $"{FirstName} {LastName}";
+}

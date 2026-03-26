ecaca9d382f63c373579a00165ec0ae460135615
Gleb Karelin <s33244@pjwstk.edu.pl>
2026-03-26 01:26:15 +0100

    Working version: rental system with all features

--- obj/Debug/net10.0/apbd-cw3-s33244-clean.AssemblyInfo.cs
+++ obj/Debug/net10.0/apbd-cw3-s33244-clean.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("apbd-cw3-s33244")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("apbd-cw3-s33244-clean")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+952dd6c2bdf8a82684b86e3362a0df08f5b7ea8a")]
-[assembly: System.Reflection.AssemblyProductAttribute("apbd-cw3-s33244")]
-[assembly: System.Reflection.AssemblyTitleAttribute("apbd-cw3-s33244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("apbd-cw3-s33244-clean")]
+[assembly: System.Reflection.AssemblyTitleAttribute("apbd-cw3-s33244-clean")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
